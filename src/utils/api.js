8cf8bb6524aa3c3c55dd78ea932569c7d2b64417
Pavel Cherkasov <pavelcherkasov@mail.ru>
2021-02-02 21:55:50 +0300

    Work #10

---
+++ src/utils/api.js
@@ -0,0 +1,115 @@
+class Api {
+    constructor ({baseUrl, token}) {
+        this._baseUrl = baseUrl;
+        this._token = token
+    };
+
+    _requestResult(res) {
+      if (res.ok) {
+      return res.json();
+     }
+     return Promise.reject(`Что-то пошло не так: ${res.status}`);
+    }
+
+    getInitialCards() {
+      return fetch (`${this._baseUrl}/cards`, {
+        headers: {
+          authorization: this._token,
+        }
+      })
+      .then((res) => this._requestResult(res))
+    }
+
+    getUserData() {
+      return fetch (`${this._baseUrl}/users/me`, {
+        headers: {
+          authorization: this._token,
+        }
+      })
+      .then((res) => this._requestResult(res))
+    }
+
+    addCard(data) {
+        return fetch(`${this._baseUrl}/cards`, {
+            method: 'POST',
+            headers: {
+                authorization: this._token,
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+              name: data.name,
+              link: data.link
+            })
+          })
+          .then((res) => this._requestResult(res))
+    }
+
+    removeCard(id) {
+      return fetch(`${this._baseUrl}/cards/${id}`, {
+        method: 'DELETE',
+        headers: {
+            authorization: this._token,
+        }
+      })
+      .then((res) => this._requestResult(res))
+    }
+    
+
+    addCardLike(id){
+      return fetch(`${this._baseUrl}/cards/likes/${id}`, {
+          method: 'PUT',
+          headers: {
+              authorization: this._token,
+          },
+        })
+        .then((res) => this._requestResult(res))
+    }
+
+    deleteCardLike(id){
+      return fetch(`${this._baseUrl}/cards/likes/${id}`, {
+          method: 'DELETE',
+          headers: {
+              authorization: this._token,
+          },
+        })
+        .then((res) => this._requestResult(res))
+    }
+
+    editProfile(data) {
+      return fetch(`${this._baseUrl}/users/me`, {
+          method: 'PATCH',
+          headers: {
+              authorization: this._token,
+              'Content-Type': 'application/json'
+          },
+          body: JSON.stringify({
+            name: data.name,
+            about: data.job 
+          })
+        })
+        .then((res) => this._requestResult(res))
+  }
+
+    changeUserAvatar(data) {
+      return fetch(`${this._baseUrl}/users/me/avatar`, {
+          method: 'PATCH',
+          headers: {
+              authorization: this._token,
+              'Content-Type': 'application/json'
+          },
+          body: JSON.stringify({
+            avatar: data.link
+          })
+        })
+        .then((res) => this._requestResult(res))
+  }
+
+}
+
+const api = new Api({
+  baseUrl: 'https://mesto.nomoreparties.co/v1/cohort-19',
+  token: 'f552dc2b-43e4-4b2f-bf48-212edce5bbdf'
+})
+
+
+export default api;
